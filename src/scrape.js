b1c6962d443538f9b00ef92e77bbb73261721d49
Alex Anghel <alexisky@gmail.com>
2023-02-19 22:41:43 +0200

    add scraper wait navigation

--- src/scrape.js
+++ src/scrape.js
@@ -3,8 +3,11 @@ const puppeteer = require('puppeteer')
 async function scrape(url, selector) {
     const browser = await puppeteer.launch({})
     const page = await browser.newPage()
+    const navigationPromise = page.waitForNavigation({waitUntil: "domcontentloaded"});
 
     await page.goto(url)
+    await navigationPromise;
+
     const element = await page.waitForSelector(selector);
     const price = await page.evaluate(element => element.textContent, element);
     await browser.close()
